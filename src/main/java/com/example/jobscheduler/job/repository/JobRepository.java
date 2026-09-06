d1a750607a0b215fa7e4b94c89cb74b95e2212d9
wassim nassour <wassim.nassour@yakeey.com>
2026-09-06 21:21:54 +0100

    feat: introduce concurrent job execution with custom thread pool configuration and optimistic locking for claims

--- src/main/java/com/example/jobscheduler/job/repository/JobRepository.java
+++ src/main/java/com/example/jobscheduler/job/repository/JobRepository.java
@@ -2,7 +2,9 @@ package com.example.jobscheduler.job.repository;
 
 import com.example.jobscheduler.job.domain.Job;
 import com.example.jobscheduler.job.enums.EJobStatus;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
@@ -24,5 +26,33 @@ public interface JobRepository extends JpaRepository<Job, Long> {
             Pageable pageable
     );
 
+
+    @Modifying
+    @Transactional
+    @Query("""
+                   update Job j
+                   set j.status = :newStatus, j.updatedAt = :now
+                   where j.id = :jobId and j.status = :pendingStatus
+            """)
+    int claimJob(
+            @Param("newStatus") EJobStatus newStatus,
+            @Param("pendingStatus") EJobStatus pendingStatus,
+            @Param("jobId") Long jobId,
+            @Param("now") Instant now
+    );
+
+    @Modifying
+    @Transactional
+    @Query("""
+                   update Job j
+                   set j.status = :pendingStatus, j.updatedAt = :now
+                   where j.id = :jobId and j.status = :runningStatus
+            """)
+    int releaseClaim(
+            @Param("jobId") Long jobId,
+            @Param("runningStatus") EJobStatus runningStatus,
+            @Param("pendingStatus") EJobStatus pendingStatus,
+            @Param("now") Instant now
+    );
 
 }
